c9a45cbc27018c8b3121c6419fc4fdd834885cef
Daniel Millett <millettdanny5@gmail.com>
2023-01-02 21:20:54 +0000

    added some extra methods

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 import time
 from datetime import datetime
 
-from gpiozero import MCP3008
-
-from sensors import check_float_switch
+from sensors import check_float_switch, read_adc
 from sensors import check_smoke_level
 
 
@@ -13,18 +11,6 @@ def create_line_protocol(sensor: str, reading: str, value):
     line: str = "{} {}={} {}"
     timestamp = str(int(datetime.now().timestamp() * 1000))
     return line.format(sensor, reading, value, timestamp)
-
-
-# read SPI data from MCP3008 chip,8 possible adc's (0 through 7)
-
-def read_adc(adcnum):
-    if (adcnum > 7) or (adcnum < 0):
-        return -1
-
-    #sensor_value = MCP3008(channel=adcnum, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
-    sensor_value = 100
-
-    return sensor_value
 
 
 # main loop
--- src/sensors.py
+++ src/sensors.py
@@ -1,4 +1,20 @@
-from gpiozero import DigitalInputDevice, BadPinFactory, MotionSensor
+from gpiozero import DigitalInputDevice, BadPinFactory, MotionSensor, MCP3008
+from pigpio_dht import DHT11
+
+# Todo have a class in main loop that initiliases all the sensors to be used
+# in main method sleep for seconds, but store in database in minutes
+
+
+# read SPI data from MCP3008 chip,8 possible adc's (0 through 7)
+
+def read_adc(adcnum):
+    if (adcnum > 7) or (adcnum < 0):
+        return -1
+
+    sensor_value = MCP3008(channel=adcnum, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
+    sensor_value = 100
+
+    return sensor_value
 
 
 def check_float_switch() -> str:
@@ -19,7 +35,7 @@ def check_smoke_level(co_level: float) -> int:
 
     if smoke_trigger > 1.5:
         print("Gas leakage")
-        #email_user("CO2")
+        # call user
         print("Current Gas AD value = " + str("%.2f" % ((co_level / 1024.) * 3.3)) + " V")
         return 0
 
@@ -31,7 +47,15 @@ def check_smoke_level(co_level: float) -> int:
 def motion_sensor() -> int:
     pir = MotionSensor(3)
 
-    if pir.is_active:
+    if pir.is_active: #fixme https://static.raspberrypi.org/files/education/posters/GPIO_Zero_Cheatsheet.pdf
         return 1
     else:
         return 0
+
+
+def humidity_and_temp() -> str:
+    sensor = DHT11(6)
+    result = sensor.read()
+
+    print(result)
+    return result
