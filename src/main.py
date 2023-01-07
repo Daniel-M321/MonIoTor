73170a6c5dfc785891199b09b9990a6a0e595c41
Daniel-M321 <millettdanny5@gmail.com>
2023-01-07 20:55:50 +0000

    connected sensors and added functionality

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import time
 from datetime import datetime
 
-from sensors import check_float_switch, read_adc
+from sensors import check_float_switch, read_adc, humidity_and_temp, motion_sensor
 from sensors import check_smoke_level
+import psutil
 
 
 # function to create line protocol needed to store in db
-
 def create_line_protocol(sensor: str, reading: str, value):
     line: str = "{} {}={} {}"
     timestamp = str(int(datetime.now().timestamp() * 1000))
@@ -15,12 +15,21 @@ def create_line_protocol(sensor: str, reading: str, value):
 
 # main loop
 def main():
+    for proc in psutil.process_iter():
+        if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+            proc.kill()
     while True:
-        fs = check_float_switch()
+        #fs = check_float_switch()
         #float_switch_data(fs)
-        time.sleep(0.1)
-        check_smoke_level(read_adc(0))
+        time.sleep(1.5)
+        humidity_and_temp()
+        if motion_sensor() == 1:
+            print("motion detected")
+        elif motion_sensor() == 0:
+            print("no motion")
         time.sleep(0.5)
+        check_smoke_level(read_adc(0))
+        time.sleep(5)
 
 
 # ---------------------------------------------------------
--- src/sensors.py
+++ src/sensors.py
@@ -1,25 +1,37 @@
+import time
+
 from gpiozero import DigitalInputDevice, BadPinFactory, MotionSensor, MCP3008
-from pigpio_dht import DHT11
+import adafruit_dht
+import board
 
 # Todo have a class in main loop that initiliases all the sensors to be used
 # in main method sleep for seconds, but store in database in minutes
 
 
-# read SPI data from MCP3008 chip,8 possible adc's (0 through 7)
+class mySensors:
+    def __init__(self):
+        try:
+            self.float_switch = DigitalInputDevice(2)
+            self.pir = MotionSensor(4, queue_len=10, sample_rate=20, threshold=0.7)
+            self.dht_sensor = adafruit_dht.DHT11(board.D27, use_pulseio=False)
+        except Exception as e:
+            print("Issue with initiliasing a sensor: ", e.args)
 
-def read_adc(adcnum):
+
+
+# read SPI data from MCP3008 chip,8 possible adc's (0 through 7)
+def read_adc(adcnum: int) -> float:
     if (adcnum > 7) or (adcnum < 0):
         return -1
 
-    sensor_value = MCP3008(channel=adcnum, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
-    sensor_value = 100
+    sensor = MCP3008(channel=adcnum, clock_pin=18, mosi_pin=15, miso_pin=17, select_pin=14)
 
-    return sensor_value
+    return sensor.value
 
 
 def check_float_switch() -> str:
     try:
-        float_switch = DigitalInputDevice(4)
+        float_switch = DigitalInputDevice(2)
     except BadPinFactory as e:
         print(e)
         return "no float switch"
@@ -40,22 +52,43 @@ def check_smoke_level(co_level: float) -> int:
         return 0
 
     else:
-        print("Gas not leak")
+        print("Gas not leak: "+str(co_level))
         return 1
 
 
 def motion_sensor() -> int:
-    pir = MotionSensor(3)
+    try:
+        pir = MotionSensor(4, queue_len=10, sample_rate=20, threshold=0.7)
+    except Exception as e:
+        print("motion sensor not detected I think", e.args)
+        return -1
 
-    if pir.is_active: #fixme https://static.raspberrypi.org/files/education/posters/GPIO_Zero_Cheatsheet.pdf
+    print(pir.value)
+    if pir.motion_detected:  # fixme https://static.raspberrypi.org/files/education/posters/GPIO_Zero_Cheatsheet.pdf
         return 1
     else:
         return 0
 
 
-def humidity_and_temp() -> str:
-    sensor = DHT11(6)
-    result = sensor.read()
+def humidity_and_temp() -> float:
+    sensor = adafruit_dht.DHT11(board.D27, use_pulseio=False)
+    hum = None
+    retries = 15
+    error = None
 
-    print(result)
-    return result
+    while retries >= 1 and hum is None:
+        try:
+            hum = sensor.humidity
+            temp = sensor.temperature
+
+            if hum is not None:
+                print('Temp=' + str(temp) + '*C \tHumidity=' + str(hum) + '%')
+        except RuntimeError as e:
+            error = "DHT failure: ", e.args
+        time.sleep(1)
+        retries -= 1
+
+    if hum is None and error:
+        print(error)
+
+    return 1
