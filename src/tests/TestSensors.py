42ae8b6a958303b8037173a7b3d5cf8701782e19
Daniel Millett <d.millett2@nuigalway.ie>
2023-01-08 22:17:11 +0000

    refactored TestSensors mock classes

--- src/tests/TestSensors.py
+++ src/tests/TestSensors.py
@@ -5,46 +5,34 @@ from src.sensors import MySensors
 
 
 class FakeDevice:
-
     def __init__(self, number: int):
         self.number = number
         self.is_active = True
         self.value = 0.9
         self.motion_detected = True
+        self.pin = number
+        self.humidity = 50
+        self.temperature = 20
+        self.D27 = 4
 
     def inactivity(self) -> None:
         self.is_active = False
         self.value = 0
         self.motion_detected = False
 
-
-class FakeDHT:
-    temperature: float
-
-    def __init__(self, pin: int):
-        self.pin = pin
-        self.humidity = 50
-        self.temperature = 20
-        self.counter = 0
-
-    # def inactive(self) -> None:
-    #     self.humidity = None
-    #     self.temperature = 0
-
-
-class FakeBoard:
-    def __init__(self):
-        self.D27 = 4
+    def activity(self) -> None:
+        self.is_active = True
+        self.value = 0.9
+        self.motion_detected = True
 
 
 class TestSensors(unittest.TestCase):
     fakeDevice = FakeDevice(4)
-    fakeDht = FakeDHT(4)
 
     @patch("src.sensors.MotionSensor", return_value=fakeDevice)
     @patch("src.sensors.DigitalInputDevice", return_value=fakeDevice)
-    @patch("src.sensors.adafruit_dht.DHT11", return_value=fakeDht)
-    @patch("src.sensors.board", return_value=FakeBoard())
+    @patch("src.sensors.adafruit_dht.DHT11", return_value=fakeDevice)
+    @patch("src.sensors.board", return_value=fakeDevice)
     def setUp(self, board_mock, dht_mock, device_mock, pir_mock):
         self.my_sensors = MySensors()
 
@@ -61,6 +49,7 @@ class TestSensors(unittest.TestCase):
     def test_float_switch_noflood(self):
         self.fakeDevice.inactivity()
         self.assertEqual(self.my_sensors.check_float_switch(), "There is no flood")
+        self.fakeDevice.activity()
 
     # smoke level tests #########
     def test_smoke_level_ok(self):
@@ -76,6 +65,7 @@ class TestSensors(unittest.TestCase):
     def test_motion_sensor_inactive(self):
         self.fakeDevice.inactivity()
         self.assertEqual(self.my_sensors.motion_sensor(), 0)
+        self.fakeDevice.activity()
 
     # dht sensor tests ###########
     def test_dht_ok(self):
