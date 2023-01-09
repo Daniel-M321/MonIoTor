05f95e177e65ed438b14d590d966f1c27e5e397b
Daniel-M321 <millettdanny5@gmail.com>
2023-01-09 16:20:58 +0000

    put env variables in place

--- src/eventhandler.py
+++ src/eventhandler.py
@@ -1,20 +1,17 @@
 import os
 from twilio.rest import Client
 
-# from dotenv import load_dotenv
-# load_dotenv()
-
 
 def text_user(sensor: str) -> None:
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    # messaging_service_sid = os.environ['MESSAGING_SERVICE_SID']
+    messaging_service_sid = os.environ['MESSAGING_SERVICE_SID']
     client = Client(account_sid, auth_token)
 
     sensor += "Join Earth's mightiest heroes. Like Kevin Bacon."
 
     message = client.messages.create(
-        messaging_service_sid='MGe9d3e7b051d7fe7ec71ba682ff2cd059',  # Todo place in env variable
+        messaging_service_sid=messaging_service_sid,
         body=sensor,
         to='+353870934553'  # Todo take user input and save
     )
@@ -23,12 +20,13 @@ def text_user(sensor: str) -> None:
 def call_user(message: str) -> None:
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    calling_number = os.environ['TWILIO_NUMBER']
     client = Client(account_sid, auth_token)
 
-    message = '<Response><Say voice="alice">'+'Hello there, this is your message'+'</Say></Response>'
+    message = '<Response><Say voice="alice">'+message+'</Say></Response>'
 
     call = client.calls.create(
         twiml=message,
         to='+353870934553',
-        from_='+19785033669'
+        from_=calling_number
     )
